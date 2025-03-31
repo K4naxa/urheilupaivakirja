0c484f21ef22f4863c0f0ed76fa6528dd65a8d3e
Juho Vasaramäki <juho.vasaramaki@edu.tampere.fi>
2025-03-31 12:27:43 +0300

    Add closing the big modal when clicking outside the modal. Fix big modal not working properly with dynamic view heights of certain browsers

--- frontend/src/components/BigModal.jsx
+++ frontend/src/components/BigModal.jsx
@@ -1,19 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
 const BigModal = ({ isOpen, onClose, content }) => {
+  const modalRef = useRef(null);
+
   useEffect(() => {
-    // ESC to close
     const handleEscape = (event) => {
       if (event.keyCode === 27) onClose();
     };
 
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        onClose();
+      }
+    };
+
     if (isOpen) {
       document.body.style.overflow = "hidden";
-
       document.addEventListener("keydown", handleEscape);
+      document.addEventListener("mousedown", handleClickOutside);
+
       return () => {
         document.removeEventListener("keydown", handleEscape);
+        document.removeEventListener("mousedown", handleClickOutside);
         document.body.style.overflow = "auto";
       };
     }
@@ -23,7 +32,10 @@ const BigModal = ({ isOpen, onClose, content }) => {
 
   return createPortal(
     <div className="fixed inset-0 z-20 flex items-start justify-center overflow-hidden bg-black bg-opacity-50">
-      <div className="w-full h-full sm:w-auto sm:h-auto sm:rounded-md sm:shadow-lg overflow-hidden flex flex-col sm:mt-[8vh]">
+      <div
+        ref={modalRef}
+        className="w-dvw h-dvh sm:w-auto sm:h-auto sm:rounded-md sm:shadow-lg overflow-hidden sm:mt-[8dvh] sm:mb-[8dvh]"
+      >
         <div className="flex flex-col h-full max-h-full sm:max-h-[90vh] overflow-auto">
           {content}
         </div>
